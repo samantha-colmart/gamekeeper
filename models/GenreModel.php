511763d9c2f4635943b5456815a6fe4f5e57804f
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-03-05 19:43:09 +0100

    feat : model genre

---
+++ models/GenreModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Models;
+
+use App\Config\Database;
+use PDO;
+
+
+class Genre extends Database {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    // Méthode pour récupérer tous les genres de jeu
+    public static function getAllGenres(): array {
+        $sql = "SELECT * FROM genre";
+        $query = $this->pdo->prepare($sql);
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Méthode pour récupérer tous les genres d'un jeu
+    public static function getGenreByGame(int $game_id): array {
+        $sql = "SELECT * FROM genre INNER JOIN game_genre ON genre.id = game_genre.genre_id WHERE game_genre.game_id = :game_id";
+        $query = $this->pdo->prepare($sql);
+        $query->execute([':game_id' => $game_id]);
+        return $query->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+?>
