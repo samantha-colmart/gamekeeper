cf728846312db6b2c5d60e626a3dd4a0ec0778f1
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-03-16 15:00:46 +0100

    feat : layout footer

---
+++ views/layout/footer.php
@@ -0,0 +1,17 @@
+</main>
+
+<footer class="footer">
+    <div class="footer-copyright">
+        <i class="fa-solid fa-gamepad"></i>
+        <p>© 2026 GameKeeper. Tous droits réservés.</p>
+    </div>
+    
+    <div class="footer-links">
+        <a href="#">Contact</a>
+        <a href="#">Conditions</a>
+        <a href="#">Support</a>
+    </div>
+</footer>
+
+</body>
+</html>
